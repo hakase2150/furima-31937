5d98fae5d04aa4ec75d169efaa85ff37873cb7c1
hakase2150 <hakase2150@gmail.com>
2021-02-15 17:23:08 +0900

    ユーザー管理機能の修正4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,23 @@ class User < ApplicationRecord
   with_options presence:true do
     validates :nickname
     validates :email
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カタカナを使用してください' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カタカナを使用してください' }
     validates :birthday
 
-    with_options uniqueness: { case_sensitive: true } do
-      validates :email
-    end
+      with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
+        validates :last_name 
+        validates :first_name
+      end
+      
+      with_options format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カタカナを使用してください' } do
+        validates :last_name_kana
+        validates :first_name_kana
+      end
+
+      with_options uniqueness: { case_sensitive: true } do
+        validates :email
+      end
     
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と数字の両方を含めて設定してください'
+      PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+      validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と数字の両方を含めて設定してください'
   end
 end
