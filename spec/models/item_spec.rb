e7f9c0e304573c6d2d7cbd5f256badb84e055fc6
hakase2150 <hakase2150@gmail.com>
2021-03-03 15:19:08 +0900

    商品出品機能の修正1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it 'priceが半角数字で¥300~9999999の間であれば登録できる' do
-        @item.price = '3000'
+        @item.price = 3000
         expect(@item).to be_valid
       end
     end
@@ -32,48 +32,88 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'categoryが空では出品できない' do
+      it 'category_idが空では出品できない' do
         @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it 'stste_idが空では出品できない' do
+      it 'category_idが---を選択している状態では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'state_idが空では出品できない' do
         @item.state_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+      it 'state_idが---を選択している状態では出品できない' do
+        @item.state_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State must be other than 1")
       end
       it 'shipping_charge_idが空では出品できない' do
         @item.shipping_charge_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
       end
+      it 'shipping_charge_idが---を選択している状態では出品できない' do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
+      end
       it 'prefecture_idが空では出品できない' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'prefecture_idが---を選択している状態では出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it 'days_to_ship_idが空では出品できない' do
         @item.days_to_ship_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
+      it 'days_to_ship_idが---を選択している状態では出品できない' do
+        @item.days_to_ship_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship must be other than 1")
+      end
       it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '販売価格が¥300以下ではないと出品できない' do
-        @item.price = '299'
+      it 'priceが¥300以下ではないと出品できない' do
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
-      it '販売価格が¥9999999以上では出品できない' do
-        @item.price = '10000000'
+      it 'priceが¥9999999以上では出品できない' do
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-      it '販売価格が全角数字では出品できない' do
+      it 'priceが全角数字では出品できない' do
         @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが全角文字では出品できない' do
+        @item.price = '三百'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが半角英数字混合では出品できない' do
+        @item.price = '12ab34'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが半角英語だけでは出品できない' do
+        @item.price = 'two thousand'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
